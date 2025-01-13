5dc1cdcbf352ed6aa4b22f4f0449a568c98833b7
Andrea <morinigoandrea7@gmail.com>
2025-01-12 22:30:35 -0500

    routing pages rendered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import Contact from "./Contact";
 import Home from "./Home";
+import About from "./About";
+import Portfolio from "./Portfolio";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
