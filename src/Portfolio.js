a547b910b0229d1ceca6ba03751679c9ea676255
Andrea <morinigoandrea7@gmail.com>
2025-01-17 14:38:01 -0500

    portfolio card rendered

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,10 +1,44 @@
 import React from "react";
 import "./Portfolio.css";
+import Footer from "./Footer";
+import painting from "./img/painting.png";
 
 export default function Portfolio() {
   return (
     <div>
-      <p>hi portfolio</p>
+      <div class="content-container container work-content mt-0">
+        <div class="content ">
+          <h1>Portfolio</h1>
+          <div class="row mt-5">
+            <div class="col-lg-3 mb-4">
+              <div class="card position-relative">
+                <img
+                  src={painting}
+                  alt="Painting project preview"
+                  class="card-img-top img-fluid"
+                />
+                <div class="card-body">
+                  <h5 class="card-title text-center">Web App</h5>
+                  <p class="card-text text-center text-muted">
+                    HTML, CSS, Javascript
+                  </p>
+                </div>
+                <div class="card-overlay">
+                  <a
+                    href="https://github.com/andreamorinigo7/painting-web-practice"
+                    target="_blank"
+                    rel="noreferrer"
+                    class="card-button"
+                  >
+                    Visit Site
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
