223e82634c6d5a555ebda86511e62a38ec0a65c6
Andrea <morinigoandrea7@gmail.com>
2025-01-23 12:48:32 -0500

    portfolio map function and array

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,40 +1,42 @@
 import React from "react";
 import "./Portfolio.css";
 import Footer from "./Footer";
-import painting from "./img/painting.png";
+import { projects } from "./Projects";
 
 export default function Portfolio() {
   return (
-    <div>
-      <div class="content-container container work-content mt-0">
-        <div class="content ">
+    <div className="portfolio">
+      <div className="content-container container work-content mt-0">
+        <div className="content">
           <h1>Portfolio</h1>
-          <div class="row mt-5">
-            <div class="col-lg-3 mb-4">
-              <div class="card position-relative">
-                <img
-                  src={painting}
-                  alt="Painting project preview"
-                  class="card-img-top img-fluid"
-                />
-                <div class="card-body">
-                  <h5 class="card-title text-center">Web App</h5>
-                  <p class="card-text text-center text-muted">
-                    HTML, CSS, Javascript
-                  </p>
-                </div>
-                <div class="card-overlay">
-                  <a
-                    href="https://github.com/andreamorinigo7/painting-web-practice"
-                    target="_blank"
-                    rel="noreferrer"
-                    class="card-button"
-                  >
-                    Visit Site
-                  </a>
+          <div className="row mt-5">
+            {projects.map((project) => (
+              <div key={project.id} className="col-lg-3 mb-4">
+                <div className="card position-relative">
+                  <img
+                    src={project.image}
+                    alt={`${project.title} project preview`}
+                    className="card-img-top img-fluid"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title text-center">{project.title}</h5>
+                    <p className="card-text text-center text-muted">
+                      {project.languages}
+                    </p>
+                  </div>
+                  <div className="card-overlay">
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="card-button"
+                    >
+                      Visit Site
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/Projects.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Weather App",
+    languages: "HTML, CSS, JavaScript, and API",
+    image: "../img/weather.png",
+    link: "https://weather-app-andi.netlify.app",
+  },
+  {
+    id: 2,
+    title: "World Clock App",
+    languages: "HTML, CSS, JavaScript, and API",
+    image: "../img/world-clock.png",
+    link: "https://world-clock-andihub.netlify.app",
+  },
+  {
+    id: 3,
+    title: "Calculator App",
+    languages: "HTML, CSS, and JavaScript",
+    image: "../img/calculator.png",
+    link: "https://github.com/andreamorinigo7/mini-pocket-calculator",
+  },
+  {
+    id: 4,
+    title: "Travel Diary Landing Page",
+    languages: "HTML and CSS",
+    image: "../img/travel.png",
+    link: "https://travel-project-andi.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Dictionary App",
+    languages: "React",
+    image: "../img/dictionary.png",
+    link: "https://dictionary-andi.netlify.app",
+  },
+  {
+    id: 6,
+    title: "Psychotherapy Website",
+    languages: "React",
+    image: "../img/psychotherapy.png",
+    link: "https://github.com/andreamorinigo7/silvia-balta-psychotherapist",
+  },
+  {
+    id: 7,
+    title: "Cinéflix",
+    languages: "React and Jikan API",
+    image: "../img/cineflix.png",
+    link: "https://cineflix-anime.netlify.app",
+  },
+];
