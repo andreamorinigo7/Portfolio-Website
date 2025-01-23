ba20dc1e64afbd14618873b2c284e3791bec55af
Andrea <morinigoandrea7@gmail.com>
2025-01-23 14:21:10 -0500

    fixed the portfolio card height

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -12,14 +12,14 @@ export default function Portfolio() {
           <div className="row mt-5 portfolio-row">
             {projects.map((project) => (
               <div key={project.id} className="col-lg-3 mb-4">
-                <div className="card position-relative">
+                <div className="card">
                   <img
                     src={project.image}
                     alt={`${project.title} project preview`}
-                    className="card-img-top img-fluid"
+                    className="card-img img-fluid"
                   />
                   <div className="card-body">
-                    <h5 className="card-title text-center">{project.title}</h5>
+                    <h5 className="card-title text-center ">{project.title}</h5>
                     <p className="card-text text-center mb-5 text-muted">
                       {project.languages}
                     </p>
